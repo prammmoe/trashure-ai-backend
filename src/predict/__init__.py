569bd19ed5162d3701d872ec2e5834323937017a
prammmoe <ikhwanpramuditha05@gmail.com>
2024-03-21 05:05:55 +0700

    refactor app to clean arch

---
+++ src/predict/__init__.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify, render_template, request
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+
+class App:
+    def __init__(self, app):
+        self.app = app
+
+        # Load model and class_names
+        self.model = tf.keras.models.load_model('../src/models/model.h5')
+        self.class_names = np.load('../src/models/nama_kelas.npy')
+        print('Model loaded. Start serving ...')
+
+        # Define routes
+        self.app.route("/", methods=["GET"])(self.home_page)
+        self.app.route("/klasifikasi", methods=["POST"])(self.predict)
+
+    def home_page(self):
+        data = {}  # Empty dictionary for initial rendering
+        # return render_template("index.html", data=data)
+        return jsonify(data) # return JSON
+
+    def predict(self):
+        if request.method == "POST":
+            uploaded_file = request.files["image"]
+            if uploaded_file is not None:
+                pil_image = Image.open(uploaded_file.stream)
+                pil_image = pil_image.convert('RGB')
+                pil_image = pil_image.resize((299, 299))
+                image_array = np.array(pil_image) / 255.
+
+                prediction = self.model.predict(np.expand_dims(image_array, axis=0))
+                index = np.argmax(prediction)
+                confidence = prediction[0][index]
+
+                if confidence >= 0.6:
+                    nama_kelas = self.class_names[index]
+                    data = {
+                        "nama_kelas": nama_kelas,
+                        "confidence": confidence*100,
+                        "jenis_sampah": self.sampah(nama_kelas, self.class_names)
+                    }
+                    # return render_template("index.html", data=data)
+                    return jsonify(data) # return JSON 
+
+                
+                else:
+                    data = {
+                        "error": "Sampah tidak ditemukan. Coba lagi."
+                    }
+                    # return render_template("index.html", data=data)
+                    return jsonify(data) # return JSON
+
+    def sampah(self, nama_kelas, nama_kelas_all):
+        sampah_dict = {
+            nama_kelas_all[0]: "kardus",
+            nama_kelas_all[1]: "kaca",
+            nama_kelas_all[2]: "kaleng",
+            nama_kelas_all[3]: "organic_fresh",
+            nama_kelas_all[4]: "organic_common",
+            nama_kelas_all[5]: "organic_rot",
+            nama_kelas_all[6]: "kertas",
+            nama_kelas_all[7]: "gelas",
+            nama_kelas_all[8]: "kantong",
+            nama_kelas_all[9]: "botol",
+            nama_kelas_all[10]: "alat_makan",
+        }
+        return sampah_dict.get(nama_kelas, "jenis_sampah_umum")
+    
+    def run(self):
+        self.app.run(debug=True, port=8001)
