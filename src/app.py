4e7b0cf4dee3f1fc0152a4858c6c60381e679375
prammmoe <ikhwanpramuditha05@gmail.com>
2024-03-21 10:04:36 +0700

    rename my_app to trashure

--- src/app.py
+++ src/app.py
@@ -4,5 +4,5 @@ from flask import Flask
 
 if __name__ == '__main__':
     app = Flask(__name__)  # Create the Flask app instance
-    my_app = App(app)  # Pass the app instance to the App constructor
-    my_app.run()  # Call the run method on the App instance
+    trashure_backend = App(app)  # Pass the app instance to the App constructor
+    trashure_backend.run()  # Call the run method on the App instance
