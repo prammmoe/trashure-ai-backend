9d6adb5e61a9f92be0155a53fe1be7b2c68b5016
prammmoe <ikhwanpramuditha05@gmail.com>
2024-04-27 21:02:00 +0700

    feat: change main class name

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-from predict import App
+from predict.prediction import Prediction
 from flask import Flask
 
 if __name__ == '__main__':
     app = Flask(__name__)  
-    trashure_backend = App(app)  # Pass the app instance to the App constructor
+    trashure_backend = Prediction(app)  # Pass the app instance to the App constructor
     trashure_backend.run()  # Call the run method on the App instance
